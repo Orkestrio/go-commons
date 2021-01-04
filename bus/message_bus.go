494a8237a3b48cfed1ff75d36eefdb086b1de0d4
sk_ <sk_@sk-s-MacBook-Pro.local>
2021-01-04 14:47:45 +0200

    [master] initial

---
+++ bus/message_bus.go
@@ -0,0 +1,44 @@
+package bus
+
+import "sync"
+
+type DataEvent struct {
+	Data  interface{}
+	Topic string
+}
+
+type DataChannel chan DataEvent
+
+type DataChannelSlice []DataChannel
+
+type EventBus struct {
+	subscribers map[string]DataChannelSlice
+	rm          sync.RWMutex
+}
+
+func (eb *EventBus) Publish(topic string, data interface{}) {
+	eb.rm.RLock()
+	if chans, found := eb.subscribers[topic]; found {
+		channels := append(DataChannelSlice{}, chans...)
+		go func(data DataEvent, dataChannelSlices DataChannelSlice) {
+			for _, ch := range dataChannelSlices {
+				ch <- data
+			}
+		}(DataEvent{Data: data, Topic: topic}, channels)
+	}
+	eb.rm.RUnlock()
+}
+
+func (eb *EventBus) Subscribe(topic string, ch DataChannel) {
+	eb.rm.Lock()
+	if prev, found := eb.subscribers[topic]; found {
+		eb.subscribers[topic] = append(prev, ch)
+	} else {
+		eb.subscribers[topic] = append([]DataChannel{}, ch)
+	}
+	eb.rm.Unlock()
+}
+
+var MessageBus = &EventBus{
+	subscribers: map[string]DataChannelSlice{},
+}
